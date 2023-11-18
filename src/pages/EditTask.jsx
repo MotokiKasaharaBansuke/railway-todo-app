f7487b8f659148dcc6254c666c001735558dee1f
笠原元輝 <kasaharamotoki@kasaharamotokinoMacBook-Air.local>
2023-11-18 16:50:34 +0900

    first commit

--- src/pages/EditTask.jsx
+++ src/pages/EditTask.jsx
@@ -3,72 +3,85 @@ import { Header } from "../components/Header";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 import { url } from "../const";
-import { useHistory, useParams } from "react-router-dom";
-import "./editTask.css"
+import { useNavigate, useParams } from "react-router-dom";
+import "./editTask.scss";
 
 export const EditTask = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
   const { listId, taskId } = useParams();
   const [cookies] = useCookies();
   const [title, setTitle] = useState("");
   const [detail, setDetail] = useState("");
+  const [limit, setLimit] = useState("");
   const [isDone, setIsDone] = useState();
   const [errorMessage, setErrorMessage] = useState("");
   const handleTitleChange = (e) => setTitle(e.target.value);
   const handleDetailChange = (e) => setDetail(e.target.value);
+  const handleLimitChange = (e) => setLimit(e.target.value);
   const handleIsDoneChange = (e) => setIsDone(e.target.value === "done");
+  function convertUTCtoJST(utcDateTime) {
+    const date = new Date(utcDateTime);
+    const jstOffset = 9 * 60 * 60 * 1000;
+    const jstDate = new Date(date.getTime() + jstOffset);
+    return jstDate.toISOString();
+  }
+
   const onUpdateTask = () => {
-    console.log(isDone)
     const data = {
       title: title,
       detail: detail,
-      done: isDone
-    }
+      done: isDone,
+      limit: limit,
+    };
 
-    axios.put(`${url}/lists/${listId}/tasks/${taskId}`, data, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then((res) => {
-      console.log(res.data)
-      history.push("/");
-    })
-    .catch((err) => {
-      setErrorMessage(`更新に失敗しました。${err}`);
-    })
-  }
+    axios
+      .put(`${url}/lists/${listId}/tasks/${taskId}`, data, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        navigate("/");
+      })
+      .catch((err) => {
+        setErrorMessage(`更新に失敗しました。${err}`);
+      });
+  };
 
   const onDeleteTask = () => {
-    axios.delete(`${url}/lists/${listId}/tasks/${taskId}`, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then(() => {
-      history.push("/");
-    })
-    .catch((err) => {
-      setErrorMessage(`削除に失敗しました。${err}`);
-    })
-  }
+    axios
+      .delete(`${url}/lists/${listId}/tasks/${taskId}`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        setErrorMessage(`削除に失敗しました。${err}`);
+      });
+  };
 
   useEffect(() => {
-    axios.get(`${url}/lists/${listId}/tasks/${taskId}`, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then((res) => {
-      const task = res.data
-      setTitle(task.title)
-      setDetail(task.detail)
-      setIsDone(task.done)
-    })
-    .catch((err) => {
-      setErrorMessage(`タスク情報の取得に失敗しました。${err}`);
-    })
-  }, [])
+    axios
+      .get(`${url}/lists/${listId}/tasks/${taskId}`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        const task = res.data;
+        setTitle(task.title);
+        setDetail(task.detail);
+        setIsDone(task.done);
+        setLimit(convertUTCtoJST(task.limit));
+      })
+      .catch((err) => {
+        setErrorMessage(`タスク情報の取得に失敗しました。${err}`);
+      });
+  }, []);
 
   return (
     <div>
@@ -77,18 +90,72 @@ export const EditTask = () => {
         <h2>タスク編集</h2>
         <p className="error-message">{errorMessage}</p>
         <form className="edit-task-form">
-          <label>タイトル</label><br />
-          <input type="text" onChange={handleTitleChange} className="edit-task-title" value={title} /><br />
-          <label>詳細</label><br />
-          <textarea type="text" onChange={handleDetailChange} className="edit-task-detail" value={detail} /><br />
+          <label>タイトル</label>
+          <br />
+          <input
+            type="text"
+            onChange={handleTitleChange}
+            className="edit-task-title"
+            value={title}
+          />
+          <br />
+          <label>詳細</label>
+          <br />
+          <textarea
+            type="text"
+            onChange={handleDetailChange}
+            className="edit-task-detail"
+            value={detail}
+          />
+          <br />
+          <label>期限</label>
+          <br />
+          <input
+            type="datetime-local"
+            name="datetime"
+            min="2023-01-01T00:00"
+            pattern="\d{4}-\d{2}-\d{2}T\d{2}:\d{2}"
+            onChange={handleLimitChange}
+            className="edit-task-limit"
+            value={limit.slice(0, -5)}
+          />
+          <br />
           <div>
-            <input type="radio" id="todo" name="status" value="todo" onChange={handleIsDoneChange} checked={isDone === false ? "checked" : ""} />未完了
-            <input type="radio" id="done" name="status" value="done" onChange={handleIsDoneChange} checked={isDone === true ? "checked" : ""} />完了
+            <input
+              type="radio"
+              id="todo"
+              name="status"
+              value="todo"
+              onChange={handleIsDoneChange}
+              checked={isDone === false ? "checked" : ""}
+            />
+            未完了
+            <input
+              type="radio"
+              id="done"
+              name="status"
+              value="done"
+              onChange={handleIsDoneChange}
+              checked={isDone === true ? "checked" : ""}
+            />
+            完了
           </div>
-          <button type="button" className="delete-task-button" onClick={onDeleteTask}>削除</button>
-          <button type="button" className="edit-task-button" onClick={onUpdateTask}>更新</button>
+          <button
+            type="button"
+            className="delete-task-button"
+            onClick={onDeleteTask}
+          >
+            削除
+          </button>
+          <button
+            type="button"
+            className="edit-task-button"
+            onClick={onUpdateTask}
+          >
+            更新
+          </button>
         </form>
       </main>
     </div>
-  )
-}
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import { Header } from "../components/Header";
 import { url } from "../const";
-import "./home.css";
-
+import "./home.scss";
 
 export const Home = () => {
   const [isDoneDisplay, setIsDoneDisplay] = useState("todo"); // todo->未完了 done->完了
@@ -15,52 +15,72 @@ export const Home = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [cookies] = useCookies();
   const handleIsDoneDisplayChange = (e) => setIsDoneDisplay(e.target.value);
+  const handleKeyDown = (e) => {
+    const currentIndex = lists.findIndex((list) => list.id === selectListId);
+    let newIndex = null;
+    if (e.key === "ArrowRight") {
+      newIndex = (currentIndex + 1) % lists.length;
+    } else if (e.key === "ArrowLeft") {
+      newIndex = (currentIndex - 1 + lists.length) % lists.length;
+    }
+
+    if (newIndex !== null) {
+      setSelectListId(lists[newIndex].id);
+      handleSelectList(lists[newIndex].id);
+    }
+  };
   useEffect(() => {
-    axios.get(`${url}/lists`, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then((res) => {
-      setLists(res.data)
-    })
-    .catch((err) => {
-      setErrorMessage(`リストの取得に失敗しました。${err}`);
-    })
+    axios
+      .get(`${url}/lists`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        setLists(res.data);
+        setSelectListId(res.data[0].id);
+      })
+      .catch((err) => {
+        setErrorMessage(`リストの取得に失敗しました。${err}`);
+      });
   }, []);
 
   useEffect(() => {
-    const listId = lists[0]?.id
-    if(typeof listId !== "undefined"){
-      setSelectListId(listId)
-      axios.get(`${url}/lists/${listId}/tasks`, {
-        headers: {
-          authorization: `Bearer ${cookies.token}`
-        }
-      })
-      .then((res) => {
-        setTasks(res.data.tasks)
-      })
-      .catch((err) => {
-        setErrorMessage(`タスクの取得に失敗しました。${err}`);
-      })
+    const listId = lists[0]?.id;
+    if (typeof listId !== "undefined") {
+      setSelectListId(listId);
+      axios
+        .get(`${url}/lists/${listId}/tasks`, {
+          headers: {
+            authorization: `Bearer ${cookies.token}`,
+          },
+        })
+        .then((res) => {
+          setTasks(res.data.tasks);
+        })
+        .catch((err) => {
+          setErrorMessage(`タスクの取得に失敗しました。${err}`);
+        });
     }
   }, [lists]);
 
   const handleSelectList = (id) => {
     setSelectListId(id);
-    axios.get(`${url}/lists/${id}/tasks`, {
-      headers: {
-        authorization: `Bearer ${cookies.token}`
-      }
-    })
-    .then((res) => {
-      setTasks(res.data.tasks)
-    })
-    .catch((err) => {
-      setErrorMessage(`タスクの取得に失敗しました。${err}`);
-    })
-  }
+    axios
+      .get(`${url}/lists/${id}/tasks`, {
+        headers: {
+          authorization: `Bearer ${cookies.token}`,
+        },
+      })
+      .then((res) => {
+        setTasks(res.data.tasks);
+        setSelectListId(id);
+      })
+      .catch((err) => {
+        setErrorMessage(`タスクの取得に失敗しました。${err}`);
+      });
+  };
+
   return (
     <div>
       <Header />
@@ -70,22 +90,32 @@ export const Home = () => {
           <div className="list-header">
             <h2>リスト一覧</h2>
             <div className="list-menu">
-              <p><Link to="/list/new">リスト新規作成</Link></p>
-              <p><Link to={`/lists/${selectListId}/edit`}>選択中のリストを編集</Link></p>
+              <p>
+                <Link to="/list/new">リスト新規作成</Link>
+              </p>
+              <p>
+                <Link to={`/lists/${selectListId}/edit`}>
+                  選択中のリストを編集
+                </Link>
+              </p>
             </div>
           </div>
-          <ul className="list-tab">
+          <ul className="list-tab" role="tablist">
             {lists.map((list, key) => {
               const isActive = list.id === selectListId;
               return (
-                <li 
+                <li
                   key={key}
                   className={`list-tab-item ${isActive ? "active" : ""}`}
                   onClick={() => handleSelectList(list.id)}
+                  role="tab"
+                  tabIndex={list.id === selectListId ? 0 : -1}
+                  onKeyDown={handleKeyDown}
+                  aria-selected={list.id === selectListId}
                 >
                   {list.title}
                 </li>
-              )
+              );
             })}
           </ul>
           <div className="tasks">
@@ -94,55 +124,92 @@ export const Home = () => {
               <Link to="/task/new">タスク新規作成</Link>
             </div>
             <div className="display-select-wrapper">
-              <select onChange={handleIsDoneDisplayChange} className="display-select">
+              <select
+                onChange={handleIsDoneDisplayChange}
+                className="display-select"
+              >
                 <option value="todo">未完了</option>
                 <option value="done">完了</option>
               </select>
             </div>
-            <Tasks tasks={tasks} selectListId={selectListId} isDoneDisplay={isDoneDisplay} />
+            <Tasks
+              tasks={tasks}
+              selectListId={selectListId}
+              isDoneDisplay={isDoneDisplay}
+            />
           </div>
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
 // 表示するタスク
 const Tasks = (props) => {
   const { tasks, selectListId, isDoneDisplay } = props;
-  if (tasks === null) return <></>
+  const calculateLimitLeft = (_limit) => {
+    const now = new Date();
+    // new Dateは実行環境元の時刻元にする。現状UTCになるためJSTに変換
+    // now.setHours(now.getHours() + 9);
+    const limit = new Date(_limit);
+    const diff = limit - now;
 
-  if(isDoneDisplay == "done"){
+    if (diff <= 0) {
+      return "期限切れ";
+    }
+    const dayLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hourLeft = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutesLeft = Math.floor((diff / (1000 * 60)) % 60);
+
+    return `期限まで${dayLeft}日${hourLeft}時間${minutesLeft}分`;
+  };
+  if (tasks === null) return <></>;
+
+  if (isDoneDisplay == "done") {
     return (
       <ul>
-        {tasks.filter((task) => {
-          return task.done === true
-        })
-        .map((task, key) => (
-          <li key={key} className="task-item">
-            <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
-              {task.title}<br />
-              {task.done ? "完了" : "未完了"}
-            </Link>
-          </li>
-        ))}
+        {tasks
+          .filter((task) => {
+            return task.done === true;
+          })
+          .map((task, key) => (
+            <li key={key} className="task-item">
+              <Link
+                to={`/lists/${selectListId}/tasks/${task.id}`}
+                className="task-item-link"
+              >
+                {task.title}
+                <br />
+                {task.done ? "完了" : "未完了"}
+                <br />
+                タスクの期限: {task.limit}
+              </Link>
+            </li>
+          ))}
       </ul>
-    )
+    );
   }
 
   return (
     <ul>
-      {tasks.filter((task) => {
-        return task.done === false
-      })
-      .map((task, key) => (
-        <li key={key} className="task-item">
-          <Link to={`/lists/${selectListId}/tasks/${task.id}`} className="task-item-link">
-            {task.title}<br />
-            {task.done ? "完了" : "未完了"}
-          </Link>
-        </li>
-      ))}
+      {tasks
+        .filter((task) => {
+          return task.done === false;
+        })
+        .map((task, key) => (
+          <li key={key} className="task-item">
+            <Link
+              to={`/lists/${selectListId}/tasks/${task.id}`}
+              className="task-item-link"
+            >
+              {task.title}
+              <br />
+              {task.done ? "完了" : "未完了"}
+              <br />
+              残り日時: {calculateLimitLeft(task.limit)}
+            </Link>
+          </li>
+        ))}
     </ul>
-  )
-}
+  );
+};
